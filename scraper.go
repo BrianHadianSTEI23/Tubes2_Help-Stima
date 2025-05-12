2ffd9875e4cf2555c2f2bfb48c5ae464a06ea1db
unknown <izzatjundy04@gmail.com>
2025-05-12 19:05:44 +0700

    feat: scraping tier

--- scraper.go
+++ scraper.go
@@ -1,4 +1,4 @@
-package scraper
+package main
 
 import (
 	"log"
@@ -8,10 +8,11 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-var elementsCombination [][]string
-var elementsImage map[string]string
+// var elementsCombination [][]string
+// var elementsImage map[string]string
+// var elementsTier map[string]int
 
-func scraper() {
+func main() {
 	url := "https://little-alchemy.fandom.com/wiki/Elements_(Little_Alchemy_2)"
 
 	// Fetch-nya berhasil ga bang
@@ -41,59 +42,68 @@ func scraper() {
 	}
 
 	elementsImage := make(map[string]string)
+	elementsTier := make(map[string]int)
 
-	doc.Find("table.list-table.col-list.icon-hover").Each(func(i int, table *goquery.Selection) {
-		table.Find("tr").Each(func(j int, row *goquery.Selection) {
-			if j == 0 {
-				return // skip header
-			}
+	doc.Find("div.mw-content-ltr.mw-parser-output").Each(func(i int, tier *goquery.Selection) {
 
-			cols := row.Find("td")
-			if cols.Length() != 2 {
-				return
-			}
+		currentTier := 0
+		tier.Find("table.list-table.col-list.icon-hover").Each(func(j int, table *goquery.Selection) {
+			// currentTier := tierList[currentTier]
+			table.Find("tr").Each(func(k int, row *goquery.Selection) {
+				if k == 0 {
+					return // skip header
+				}
 
-			imageLink := ""
-			row.Find("span span a").First().Each(func(_ int, a *goquery.Selection) {
-				if href, exists := a.Attr("href"); exists {
-					if idx := strings.Index(href, ".svg"); idx != -1 {
-						imageLink = href[:idx+len(".svg")]
+				cols := row.Find("td")
+				if cols.Length() != 2 {
+					return
+				}
+
+				imageLink := ""
+				row.Find("span span a").First().Each(func(_ int, a *goquery.Selection) {
+					if href, exists := a.Attr("href"); exists {
+						if idx := strings.Index(href, ".svg"); idx != -1 {
+							imageLink = href[:idx+len(".svg")]
+						}
+					}
+				})
+
+				elementName := strings.TrimSpace(cols.Eq(0).Text())
+				combinationCell := cols.Eq(1)
+
+				elementsImage[elementName] = imageLink
+				elementsTier[elementName] = currentTier
+
+				var combinations []string
+				if combinationCell.Find("li").Length() > 0 {
+					combinationCell.Find("li").Each(func(_ int, li *goquery.Selection) {
+						combinations = append(combinations, strings.TrimSpace(li.Text()))
+					})
+				} else {
+					raw := strings.TrimSpace(combinationCell.Text())
+					if raw != "" {
+						combinations = append(combinations, raw)
+					}
+				}
+
+				for _, combo := range combinations {
+					parts := strings.Split(combo, "+")
+					if len(parts) == 2 {
+						ing1 := strings.TrimSpace(parts[0])
+						ing2 := strings.TrimSpace(parts[1])
+						if len(ing1) <= 20 {
+							elementsCombination = append(elementsCombination, []string{elementName, ing1, ing2})
+						}
+					} else {
+						ing := strings.TrimSpace(combo)
+						if len(ing) <= 20 {
+							elementsCombination = append(elementsCombination, []string{elementName, ing, ""})
+						}
 					}
 				}
 			})
 
-			elementName := strings.TrimSpace(cols.Eq(0).Text())
-			combinationCell := cols.Eq(1)
-
-			elementsImage[elementName] = imageLink
-
-			var combinations []string
-			if combinationCell.Find("li").Length() > 0 {
-				combinationCell.Find("li").Each(func(_ int, li *goquery.Selection) {
-					combinations = append(combinations, strings.TrimSpace(li.Text()))
-				})
-			} else {
-				raw := strings.TrimSpace(combinationCell.Text())
-				if raw != "" {
-					combinations = append(combinations, raw)
-				}
-			}
-
-			for _, combo := range combinations {
-				parts := strings.Split(combo, "+")
-				if len(parts) == 2 {
-					ing1 := strings.TrimSpace(parts[0])
-					ing2 := strings.TrimSpace(parts[1])
-					if len(ing1) <= 20 {
-						elementsCombination = append(elementsCombination, []string{elementName, ing1, ing2})
-					}
-				} else {
-					ing := strings.TrimSpace(combo)
-					if len(ing) <= 20 {
-						elementsCombination = append(elementsCombination, []string{elementName, ing, ""})
-					}
-				}
-			}
+			currentTier += 1
 		})
 	})
 
